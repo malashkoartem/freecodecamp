866a87e38f35b492b3b6a8982f6bd67de0e499db
malashkoartem <malashkoartem@yandex.ru>
2022-02-04 15:42:13 +0300

    test

---
+++ 21.js
@@ -0,0 +1,10 @@
+let a = 3;
+let b = 17;
+let c = 12;
+a+=12;
+b+=9;
+c+=7;
+// Only change code below this line
+a = 3 + 12;
+b = 9 + 17;
+c = 12 + 7;
---
+++ 22.js
@@ -0,0 +1,10 @@
+let a = 11;
+let b = 9;
+let c = 3;
+a-=6;
+b-=-6;
+c-=2;
+// Only change code below this line
+a = 11 - 6;
+b = 9 - 15;
+c = 3 - 1;
