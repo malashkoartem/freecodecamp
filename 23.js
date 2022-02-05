093c098088be695133af7162fcf17bdb4b54194e
malashkoartem <malashkoartem@yandex.ru>
2022-02-05 14:27:08 +0300

    test

---
+++ 23.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 12;
+let c = 4.6;
+a*=5;
+b*=3
+c*=10
+// Only change code below this line
+a = 5 * 5;
+b = 3 * 12;
+c = 4.6 * 10;
---
+++ 24.js
@@ -0,0 +1,10 @@
+let a = 48;
+let b = 108;
+let c = 33;
+a/=12;
+b/=4;
+c/=11;
+// Only change code below this line
+a = 48 / 12;
+b = 108 / 4;
+c = 33 / 11;
